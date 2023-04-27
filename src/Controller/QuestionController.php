57ab5033d903ea21032abf601e857a4fa1f9340f
Pouish85 <pouish.iphone@gmail.com>
2023-04-27 10:12:59 +0200

    feat: affichage de la liste des question sur l'accueil

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -11,7 +11,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class QuestionController extends AbstractController
 {
     #[Route('/question/ask', name: 'ask_question')]
-    public function index(Request $request): Response
+    public function ask(Request $request): Response
     {
         $formQuestion = $this->createForm((QuestionType::class));
         $formQuestion->handleRequest($request);
