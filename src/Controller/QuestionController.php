c8688a6b793611b1a13e7fb19e7772a569662f36
Pouish85 <pouish.iphone@gmail.com>
2023-05-05 10:21:29 +0200

    feat: mise en place des votes pour les questions / reponses

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -52,6 +52,8 @@ class QuestionController extends AbstractController
             $comment->setRating(0);
             $comment->setQuestion($question);
 
+            $question->setNbResponse($question->getNbResponse() + 1);
+
             $em->persist($comment);
             $em->flush();
 
@@ -62,5 +64,15 @@ class QuestionController extends AbstractController
 
 
         return $this->render('question/show.html.twig', ['question' => $question, 'form' => $commentForm->createView()]);
+    }
+
+    #[Route('/question/rating/{id}/{score}', name: 'question_rating')]
+    public function rate(Request $request, Question $question, int $score, EntityManagerInterface $em)
+    {
+        $question->setRating($question->getRating() + $score);
+        $em->flush();
+
+        $referer = $request->server->get('HTTP_REFERER');
+        return $referer ? $this->redirect($referer) : $this->redirectToRoute('home');
     }
 }
