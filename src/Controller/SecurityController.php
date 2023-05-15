f4e9328bb13969d599132bddaf2b376e19863992
Pouish85 <pouish.iphone@gmail.com>
2023-05-15 17:41:38 +0200

    feat: mise en place du formulaire pour la réinitialisation du mot de passe

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,12 +2,18 @@
 
 namespace App\Controller;
 
+use App\Entity\ResetPassword;
 use App\Entity\User;
 use App\Form\UserType;
+use App\Repository\ResetPasswordRepository;
+use App\Repository\UserRepository;
+use DateTimeImmutable;
 use DateTimeZone;
+use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Mailer\MailerInterface;
@@ -15,6 +21,8 @@ use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use Symfony\Component\Security\Http\Authentication\UserAuthenticatorInterface;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class SecurityController extends AbstractController
 {
@@ -72,6 +80,70 @@ class SecurityController extends AbstractController
 
     #[Route('/logout', name: 'logout')]
     public function logout()
+    {
+    }
+
+    #[Route('/reset-password-request', name: 'reset-password-request')]
+    public function resetPasswordRequest(Request $request, UserRepository $userRepository, EntityManagerInterface $em, ResetPasswordRepository $resetPasswordRepository, MailerInterface $mailerInterface)
+    {
+        $emailForm = $this->createFormBuilder()
+            ->add('email', EmailType::class, [
+                'constraints' => [
+                    new NotBlank([
+                        'message' => "Veuillez renseigner ce champ."
+                    ]),
+                    new Email([
+                        'message' => 'Veuillez entre un e-mail valide.'
+                    ])
+                ]
+            ])
+            ->getForm();
+
+        $emailForm->handleRequest($request);
+
+        if ($emailForm->isSubmitted() && $emailForm->isValid()) {
+            $email = $emailForm->get('email')->getData();
+            $user = $userRepository->findOneBy(['email' => $email]);
+
+            if ($user) {
+                $oldResetPassword = $resetPasswordRepository->findOneBy(['user' => $user]);
+                if ($oldResetPassword) {
+                    $em->remove($oldResetPassword);
+                    $em->flush();
+                }
+
+
+                $token = substr(str_replace(['+', '/', '='], '', base64_encode(random_bytes(40))), 0, 20);
+
+                $resetPassword = new ResetPassword();
+                $resetPassword->setUser($user)
+                    ->setToken($token)
+                    ->setExpiredAt(new DateTimeImmutable('+2 hours'));
+
+                $em->persist($resetPassword);
+                $em->flush();
+
+                $resetEmail = new TemplatedEmail();
+                $resetEmail->to($email)
+                    ->subject('Demande de réinitialisation de mot de passe')
+                    ->htmlTemplate('@email_templates/reset-password-request.html.twig')
+                    ->context([
+                        'username' => $user->getUsername(),
+                        'token' => $token
+                    ]);
+                $mailerInterface->send($resetEmail);
+            }
+
+
+            $this->addFlash('success', "Si l'adresse email entrée est enregistrée, un email vous a été envoyé");
+            return $this->redirectToRoute('signin');
+        }
+
+        return $this->render('security/reset-password-request.html.twig', ['form' => $emailForm->createView()]);
+    }
+
+    #[Route('/reset-password/{token}', name: 'reset-password')]
+    public function resetPassword()
     {
     }
 }
